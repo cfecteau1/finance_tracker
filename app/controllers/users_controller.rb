1fe11abbe7a0781d896e9cb1131567a1f4b38dac
cfecteau1@icloud.com <cfecteau1@icloud.com>
2016-04-05 14:33:04 +0000

    add user and stock association and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def my_portfolio
-    
+    @user_stocks = current_user.stocks 
+    @user = current_user # restricts actions based on having @user as current user
   end
   
 end
